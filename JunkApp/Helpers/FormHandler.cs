093db17a76d01378eef4d04c335c1998e5f86f22
Jason Wilson <jwilsonjx@outlook.com>
2017-05-10 11:24:22 -0400

    Add project files.

---
+++ JunkApp/Helpers/FormHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkApp.Helpers
+{
+    public class FormHandler
+    {
+        private readonly IMailSender mailsender;
+        public FormHandler(IMailSender mailSender)
+        {
+            this.mailsender = mailsender;
+        }
+
+        public void Handle(string toAdress)
+        {
+            IMailSender mailSender = new MockMailSender();
+            mailSender.Send(toAdress, "This is non-Ninject exmaple");
+        }
+    }
+}
---
+++ JunkApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JunkApp.Helpers;
+using Ninject;
+using System.Reflection;
+
+namespace JunkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            var mailSender = kernel.Get<IMailSender>();
+
+            var formHandler = new FormHandler(mailSender);
+            formHandler.Handle("address2@domain.com");
+
+            Console.ReadLine();          
+        }
+    }
+}
